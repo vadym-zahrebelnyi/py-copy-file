4c6b135ddaaf84fedc874a4c4b5ce0cd1c07c501
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-11-04 20:14:20 +0200

    Added suggested changes

--- app/main.py
+++ app/main.py
@@ -5,12 +5,11 @@ def copy_file(command: str) -> None:
     if (
         len(command_chain := command.split(" ")) == 3
         and command_chain[0] == "cp"
-        and os.path.exists(command_chain[1])
-        and command_chain[1] != command_chain[2]
+        and os.path.exists(src := command_chain[1])
+        and src != (dst := command_chain[2])
     ):
         with (
-            open(command_chain[1], "r") as file_in,
-            open(command_chain[2], "w") as file_out
+            open(src, "r") as file_in,
+            open(dst, "w") as file_out
         ):
             file_out.write(file_in.read())
-            print("Copy created")
